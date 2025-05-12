2b4579ca65655828e54a6b7bed143cf2e42857f2
Wakandee <koechallanhi@gmail.com>
2025-05-12 12:27:38 +0300

    Implement logout functionality with session cleanup and route handling

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -2,7 +2,7 @@
     <h1>Kinde ERP</h1>
     <nav>
         <a href="<?= htmlspecialchars($base_url, ENT_QUOTES, 'UTF-8') ?>">Home</a>
-         <a href="<?= $base_url ?>logout" class="logout">Logout</a>
+        <a href="<?= $base_url ?>logout" class="logout">Logout</a>
     </nav>
     <button id="toggle-dark">Toggle Dark Mode</button>
 
